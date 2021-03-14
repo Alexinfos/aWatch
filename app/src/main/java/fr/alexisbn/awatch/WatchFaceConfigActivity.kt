c7c8a0ab8a7ee5e0fe4d20fe806f0e52c47a1cd1
Alexis Brandner <alexinfos@free.fr>
2021-03-14 18:51:35 +0100

    Starting work on Color and Complication selection

--- app/src/main/java/fr/alexisbn/awatch/WatchFaceConfigActivity.kt
+++ app/src/main/java/fr/alexisbn/awatch/WatchFaceConfigActivity.kt
@@ -1,7 +1,11 @@
 package fr.alexisbn.awatch
 
+import android.content.ComponentName
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.provider.Settings
+import android.support.wearable.complications.ComplicationData
+import android.support.wearable.complications.ComplicationHelperActivity
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -9,14 +13,18 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.wear.widget.WearableLinearLayoutManager
 import androidx.wear.widget.WearableRecyclerView
-import kotlinx.android.synthetic.main.activity_watch_face_config.*
+import fr.alexisbn.awatch.databinding.ActivityWatchFaceConfigBinding
+import org.jraf.android.androidwearcolorpicker.ColorPickActivity
 
 
 class WatchFaceConfigActivity : AppCompatActivity() {
     private val TAG = "WatchFaceConfigActivity"
+
+    private lateinit var binding: ActivityWatchFaceConfigBinding
 
     private lateinit var recyclerView: WearableRecyclerView
     private lateinit var viewAdapter: SettingsListAdapter
@@ -25,29 +33,93 @@ class WatchFaceConfigActivity : AppCompatActivity() {
 
     var settingsItems = arrayListOf<Triple<String, Int, String>>()
 
+    var selectedColor = Color.GREEN
+    var selectedComplicationForSlot1 = -1
+    var selectedComplicationForSlot2 = -1
+    var selectedComplicationForSlot3 = -1
+
+    private val supportedComplicationTypes = intArrayOf(
+        ComplicationData.TYPE_RANGED_VALUE,
+        ComplicationData.TYPE_ICON,
+        ComplicationData.TYPE_SHORT_TEXT,
+        ComplicationData.TYPE_SMALL_IMAGE
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_watch_face_config)
+        binding = ActivityWatchFaceConfigBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        settingsItems.add(Triple("Couleur", R.drawable.ic_baseline_color_lens_24, "ACCENT_SELECTOR"))
-        settingsItems.add(Triple("Complication 1", R.drawable.ic_baseline_one_24, "FIRST_COMPLICATION_SELECTOR"))
-        settingsItems.add(Triple("Complication 2", R.drawable.ic_baseline_two_24, "SECOND_COMPLICATION_SELECTOR"))
-        settingsItems.add(Triple("Complication 3", R.drawable.ic_baseline_3_24, "THIRD_COMPLICATION_SELECTOR"))
+        settingsItems.add(
+            Triple(
+                "Couleur",
+                R.drawable.ic_baseline_color_lens_24,
+                "ACCENT_SELECTOR"
+            )
+        )
+        settingsItems.add(
+            Triple(
+                "Complication 1",
+                R.drawable.ic_baseline_one_24,
+                "FIRST_COMPLICATION_SELECTOR"
+            )
+        )
+        settingsItems.add(
+            Triple(
+                "Complication 2",
+                R.drawable.ic_baseline_two_24,
+                "SECOND_COMPLICATION_SELECTOR"
+            )
+        )
+        settingsItems.add(
+            Triple(
+                "Complication 3",
+                R.drawable.ic_baseline_3_24,
+                "THIRD_COMPLICATION_SELECTOR"
+            )
+        )
 
         viewAdapterListener = object : SettingsListAdapter.Callback {
             override fun handleFeature(featureName: String) {
                 when (featureName) {
                     "ACCENT_SELECTOR" -> {
                         Log.d(TAG, "couleur")
+                        val intent =
+                            ColorPickActivity.IntentBuilder().oldColor(selectedColor).build(
+                                view.context
+                            )
+                        startActivityForResult(intent, 4242)
                     }
                     "FIRST_COMPLICATION_SELECTOR" -> {
                         Log.d(TAG, "C1")
+                        val intent = ComplicationHelperActivity.createProviderChooserHelperIntent(
+                            this@WatchFaceConfigActivity, ComponentName(
+                                view.context,
+                                MyWatchFace::class.java
+                            ), selectedComplicationForSlot1, *supportedComplicationTypes
+                        )
+                        startActivityForResult(intent, 20561)
                     }
                     "SECOND_COMPLICATION_SELECTOR" -> {
                         Log.d(TAG, "C2")
+                        val intent = ComplicationHelperActivity.createProviderChooserHelperIntent(
+                            this@WatchFaceConfigActivity, ComponentName(
+                                view.context,
+                                MyWatchFace::class.java
+                            ), selectedComplicationForSlot2, *supportedComplicationTypes
+                        )
+                        startActivityForResult(intent, 20562)
                     }
                     "THIRD_COMPLICATION_SELECTOR" -> {
                         Log.d(TAG, "C3")
+                        val intent = ComplicationHelperActivity.createProviderChooserHelperIntent(
+                            this@WatchFaceConfigActivity, ComponentName(
+                                view.context,
+                                MyWatchFace::class.java
+                            ), selectedComplicationForSlot3, *supportedComplicationTypes
+                        )
+                        startActivityForResult(intent, 20563)
                     }
                     else -> {
                         Log.d(TAG, "unknown")
@@ -57,17 +129,38 @@ class WatchFaceConfigActivity : AppCompatActivity() {
         }
 
         viewManager = WearableLinearLayoutManager(this)
-        viewAdapter = SettingsListAdapter(settingsItems, viewAdapterListener)
+        viewAdapter = SettingsListAdapter(settingsItems, selectedColor, viewAdapterListener)
 
-        settingsRecyclerView.apply {
+        binding.settingsRecyclerView.apply {
             layoutManager = viewManager
             adapter = viewAdapter
             setHasFixedSize(true)
+
+            isEdgeItemsCenteringEnabled = true
+            isCircularScrollingGestureEnabled = true
+            requestFocus()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            4242 -> {
+                if (resultCode != RESULT_CANCELED) {
+                    // TODO: update color.
+                    selectedColor = ColorPickActivity.getPickedColor(data!!)
+                    viewAdapter.updateSelectedAccentColor(selectedColor)
+                }
+            }
+            20561 -> {
+
+            }
         }
     }
 
     class SettingsListAdapter(
         private var settingsItems: ArrayList<Triple<String, Int, String>>,
+        private var selectedAccentColor: Int,
         private var callback: Callback?
     ) : RecyclerView.Adapter<SettingsListAdapter.ViewHolder>() {
 
@@ -106,10 +199,19 @@ class WatchFaceConfigActivity : AppCompatActivity() {
             holder.titleTextView.text = settingsItems[pos].first
             holder.iconImageView.setImageResource(settingsItems[pos].second)
 
+            if (settingsItems[pos].third == "ACCENT_SELECTOR") {
+                DrawableCompat.setTint(holder.iconImageView.drawable, selectedAccentColor)
+            }
+
             holder.itemView.setOnClickListener {
                 Log.d(TAG, "Element $pos clicked.")
                 callback?.handleFeature(settingsItems[pos].third)
             }
+        }
+
+        fun updateSelectedAccentColor(newAccentColor: Int) {
+            selectedAccentColor = newAccentColor
+            notifyDataSetChanged()
         }
 
         // Return the size of the dataset (invoked by the layout manager)
